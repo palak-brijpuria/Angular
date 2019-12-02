d732dda0e214752ba0ad72a6e60e7e786a5cb43f
palak <palakbrijpuria2208@gmail.com>
2019-12-03 00:46:50 +0530

    2nd Commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,8 @@ export class ProductService {
   constructor(public helper : HttpClient)
    { }
 
-   url = "http://172.18.5.31:9898/product"
+   //url = "http://172.18.5.31:9898/product"
+	url = "http://192.168.2.15:9898/product"
 
    getProduct()
    {
